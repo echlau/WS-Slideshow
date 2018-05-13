b5bbafb66c2861eb287d2034b88505a94372c1e7
echlau <ericlau96@hotmail.com>
2018-05-13 04:48:11 -0700

    Added functionality of creating and browsing for folders

--- WS-Slideshow/Form1.Designer.cs
+++ WS-Slideshow/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowse = new System.Windows.Forms.Button();
+            this.folderPath = new System.Windows.Forms.TextBox();
+            this.createFolders = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowse
+            // 
+            this.folderBrowse.Location = new System.Drawing.Point(639, 38);
+            this.folderBrowse.Name = "folderBrowse";
+            this.folderBrowse.Size = new System.Drawing.Size(83, 23);
+            this.folderBrowse.TabIndex = 0;
+            this.folderBrowse.Text = "Browse";
+            this.folderBrowse.UseVisualStyleBackColor = true;
+            this.folderBrowse.Click += new System.EventHandler(this.folderBrowse_Click);
+            // 
+            // folderPath
+            // 
+            this.folderPath.Location = new System.Drawing.Point(37, 40);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.Size = new System.Drawing.Size(580, 20);
+            this.folderPath.TabIndex = 1;
+            this.folderPath.Text = "C:\\Users\\EricL\\OneDrive";
+            // 
+            // createFolders
+            // 
+            this.createFolders.Location = new System.Drawing.Point(639, 67);
+            this.createFolders.Name = "createFolders";
+            this.createFolders.Size = new System.Drawing.Size(83, 23);
+            this.createFolders.TabIndex = 2;
+            this.createFolders.Text = "Create Folders";
+            this.createFolders.UseVisualStyleBackColor = true;
+            this.createFolders.Click += new System.EventHandler(this.createFolders_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createFolders);
+            this.Controls.Add(this.folderPath);
+            this.Controls.Add(this.folderBrowse);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button folderBrowse;
+        private System.Windows.Forms.TextBox folderPath;
+        private System.Windows.Forms.Button createFolders;
     }
 }
 
--- WS-Slideshow/Form1.cs
+++ WS-Slideshow/Form1.cs
@@ -6,15 +6,48 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WS_Slideshow
 {
     public partial class Form1 : Form
     {
+        private static String slideShowFolderPath;
         public Form1()
         {
             InitializeComponent();
+        }
+        //Browse for folder where slides will be held
+        private void folderBrowse_Click(object sender, EventArgs e)
+        {
+            //Displays dialog for user to select folder
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //Sets the folderPath textbox to the path selected
+                folderPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void createFolders_Click(object sender, EventArgs e)
+        {
+            //Sets static variable slideShowFolderPath to the folder to where the slides will be held
+            slideShowFolderPath = folderPath.Text;
+            if (Directory.Exists(slideShowFolderPath))
+            //Creates folders to use for different panels in the slide show
+            {
+                for (int i = 1; i <= 4; i++)
+                {
+                    Directory.CreateDirectory(slideShowFolderPath + "\\WS-Slideshow\\panel" + i);
+                }
+            }else
+            {
+                //Error for an incorrect file path
+                Debug.Print("Incorrect file path");
+            }
+
         }
     }
 }
